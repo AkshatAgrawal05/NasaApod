f62c480ea063464a48d9d352368b921cd5374b25
Akshat Agrawal <akshatagrawal58@gmail.com>
2021-04-25 14:32:31 +0530

    Full Image layout on Image Click

--- app/src/main/java/gov/nasa/apod/ui/MainActivity.kt
+++ app/src/main/java/gov/nasa/apod/ui/MainActivity.kt
@@ -1,10 +1,15 @@
 package gov.nasa.apod.ui
 
+import android.app.Dialog
 import android.os.Bundle
 import android.util.Log
+import android.view.Window
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import gov.nasa.apod.R
@@ -14,6 +19,7 @@ import gov.nasa.apod.utils.getDateFromMilliSeconds
 import gov.nasa.apod.utils.getMillisWithTimeZone
 import gov.nasa.apod.utils.isConnected
 import java.util.*
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -27,6 +33,39 @@ class MainActivity : AppCompatActivity() {
         mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setUpObserver()
         checkInternetAndCallData()
+        setUpViews()
+    }
+
+    private fun setUpViews() {
+        mainBinding.imageView.setOnClickListener() {
+            if (res != null && !res.hdUrl.isNullOrEmpty()) {
+                showFullImage(res.hdUrl!!)
+            }
+        }
+    }
+
+    private fun showFullImage(hdUrl: String) {
+        val nagDialog = Dialog(
+            this,
+            R.style.Theme_AppCompat_DialogWhenLarge
+        )
+        //R.style.Theme_Translucent_NoTitleBar_Fullscreen
+        nagDialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        nagDialog.setCancelable(false)
+        nagDialog.setContentView(R.layout.image_layout)
+        val btnClose: Button = nagDialog.findViewById(R.id.btnIvClose) as Button
+        val imageView: ImageView = nagDialog.findViewById(R.id.ivFullImage) as ImageView
+
+        Glide.with(imageView.context)
+            .load(hdUrl)
+            .placeholder(R.drawable.ic_baseline_cloud_download_24)
+            .into(imageView)
+
+        btnClose.setOnClickListener() {
+            nagDialog.cancel()
+        }
+
+        nagDialog.show()
     }
 
     private fun setUpObserver() {
